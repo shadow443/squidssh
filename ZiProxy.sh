646a1309184f302ebf1e15df170a98b3d58c3b1c
shadow443 <juan.speed.2013@gmail.com>
2020-06-15 23:55:41 -0300

    Update ZiProxy.sh

--- ZiProxy.sh
+++ ZiProxy.sh
@@ -1,15 +1,15 @@
 #/bin/bash
 
-echo -e "\ 033 [1; 33m- - - - -> \ 033 [01; 34m Iniciou a configuracao do ZiProxy, Porta 8080 CONNECT"
+echo -e "\ 033 [1; 33m- - - - -> \ 033 [01; 34m Configuracion de ZiProxy, Puerto 8081 CONNECT"
 echo -e "\ 033 [1; 33m #################"
 sleep 3
 
 apt-get update -y
-clear
+
 apt-get upgrade -y
-clear
+
 apt-get install ziproxy -y
-clear
+
 
 cd /etc/ziproxy/
 mv ziproxy.conf ziproxy.conf1
@@ -17,7 +17,7 @@ mv ziproxy.conf ziproxy.conf1
 #Puerto: 8081
 wget https://raw.githubusercontent.com/shadow443/mytest/master/ziproxy.conf
 /etc/init.d/ziproxy restart
-clear
+
 
 echo -e "\ 033 [1; 31mCANAL @PayloadHTTP"
 echo -e "\ 033 [1; 31mBy: LindoFuLL"
